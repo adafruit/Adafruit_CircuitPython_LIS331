de4f32b06bdb89cce893622990c17585e2430c0c
siddacious <nospam187+github@gmail.com>
2020-05-11 19:28:45 -0700

    accel working

--- adafruit_lis331.py
+++ adafruit_lis331.py
@@ -54,8 +54,22 @@ __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_LIS331.git"
 import adafruit_bus_device.i2c_device as i2c_device
 from adafruit_register.i2c_struct import ROUnaryStruct  # , Struct
 
-# from adafruit_register.i2c_bits import RWBits
+from adafruit_register.i2c_bits import RWBits, ROByteArray
+
 # from adafruit_register.i2c_bit import RWBit
+
+_LIS331_DEFAULT_ADDRESS = 0x18  # If SDO/SA0 is 3V, its 0x19
+_LIS331_CHIP_ID = 0x32  # The default response to WHO_AM_I for the H3LIS331 and LIS331HH
+_LIS331_REG_WHOAMI = 0x0F  # Device identification register. [0, 0, 1, 1, 0, 0, 1, 1] */
+_LIS331_REG_CTRL1 = 0x20  # Power mode, data rate, axis enable
+_LIS331_REG_CTRL2 = 0x21  # Memory reboot, HPF config
+_LIS331_REG_CTRL3 = 0x22  # Interrupt config, poarity, pin mode, latching, pin enable
+_LIS331_REG_CTRL4 = 0x23  # BDU, Endianness, Range, SPI mode
+_LIS331_REG_HP_FILTER_RESET = 0x25  # Dummy register to reset filter
+_LIS331_REG_REFERENCE = 0x26  # HPF reference value
+_LIS331_REG_OUT_X_L = 0x28  # X-axis acceleration data. Low value */
+
+_G_TO_ACCEL = 9.80665
 
 
 class CV:
@@ -79,46 +93,15 @@ class CV:
         return value in cls.string
 
 
-class AccelRange(CV):
-    """Options for ``accelerometer_range``"""
-
-
-AccelRange.add_values(
-    (
-        ("RANGE_2G", 0, 2, 0.061),
-        ("RANGE_16G", 1, 16, 0.488),
-        ("RANGE_4G", 2, 4, 0.122),
-        ("RANGE_8G", 3, 8, 0.244),
-    )
-)
-
-
 class LIS331HHRange(CV):
     """Options for ``accelerometer_range``"""
 
 
 LIS331HHRange.add_values(
     (
-        ("RANGE_2G", 0, 2, 0.061),
-        ("RANGE_16G", 1, 16, 0.488),
-        ("RANGE_4G", 2, 4, 0.122),
-        ("RANGE_8G", 3, 8, 0.244),
-    )
-)
-
-
-class H3LIS331Range(CV):
-    """Options for ``range``"""
-
-
-H3LIS331Range.add_values(
-    (
-        ("RANGE_125_DPS", 125, 125, 4.375),
-        ("RANGE_250_DPS", 0, 250, 8.75),
-        ("RANGE_500_DPS", 1, 500, 17.50),
-        ("RANGE_1000_DPS", 2, 1000, 35.0),
-        ("RANGE_2000_DPS", 3, 2000, 70.0),
-        ("RANGE_4000_DPS", 4000, 4000, 140.0),
+        ("RANGE_6G", 0, 6, ((6 * 2) / 4096) * _G_TO_ACCEL),
+        ("RANGE_12G", 1, 12, ((12 * 2) / 4096) * _G_TO_ACCEL),
+        ("RANGE_24G", 3, 24, ((24 * 2) / 4096) * _G_TO_ACCEL),
     )
 )
 
@@ -130,41 +113,35 @@ class Rate(CV):
 Rate.add_values(
     (
         ("RATE_SHUTDOWN", 0, 0, None),
-        ("RATE_12_5_HZ", 1, 12.5, None),
-        ("RATE_26_HZ", 2, 26.0, None),
-        ("RATE_52_HZ", 3, 52.0, None),
-        ("RATE_104_HZ", 4, 104.0, None),
-        ("RATE_208_HZ", 5, 208.0, None),
-        ("RATE_416_HZ", 6, 416.0, None),
-        ("RATE_833_HZ", 7, 833.0, None),
-        ("RATE_1_66K_HZ", 8, 1066.0, None),
-        ("RATE_3_33K_HZ", 9, 3033.0, None),
-        ("RATE_6_66K_HZ", 10, 6066.0, None),
-        ("RATE_1_6_HZ", 11, 1.6, None),
+        ("RATE_50_HZ", 0x4, 50, None),
+        ("RATE_100_HZ", 0x5, 100, None),
+        ("RATE_400_HZ", 0x6, 400, None),
+        ("RATE_1000_HZ", 0x7, 1000, None),
+        ("RATE_LOWPOWER_0_5_HZ", 0x8, 0.5, None),
+        ("RATE_LOWPOWER_1_HZ", 0xC, 1, None),
+        ("RATE_LOWPOWER_2_HZ", 0x10, 2, None),
+        ("RATE_LOWPOWER_5_HZ", 0x14, 5, None),
+        ("RATE_LOWPOWER_10_HZ", 0x18, 10, None),
     )
 )
 
-# /**
-#  * @brief Mode Options
-#  *
-#  */
-# typedef enum {
-#   LIS331_MODE_SHUTDOWN,
-#   LIS331_MODE_NORMAL,
-#   LIS331_MODE_LOW_POWER // Low power is from 2-6 so checks against this should
-#                         // be 'mode >=LIS331_MODE_LOW_POWER'
-# } lis331_mode_t;
-# /*!
-_LIS331_DEFAULT_ADDRESS = 0x18  # If SDO/SA0 is 3V, its 0x19
-_LIS331_CHIP_ID = 0x32  # The default response to WHO_AM_I for the H3LIS331 and LIS331HH
-_LIS331_REG_WHOAMI = 0x0F  # Device identification register. [0, 0, 1, 1, 0, 0, 1, 1] */
-_LIS331_REG_CTRL1 = 0x20  # Power mode, data rate, axis enable
-_LIS331_REG_CTRL2 = 0x21  # Memory reboot, HPF config
-_LIS331_REG_CTRL3 = 0x22  # Interrupt config, poarity, pin mode, latching, pin enable
-_LIS331_REG_CTRL4 = 0x23  # BDU, Endianness, Range, SPI mode
-_LIS331_REG_HP_FILTER_RESET = 0x25  # Dummy register to reset filter
-_LIS331_REG_REFERENCE = 0x26  # HPF reference value
-_LIS331_REG_OUT_X_L = 0x28  # X-axis acceleration data. Low value */
+
+class Mode(CV):
+    """Options for ``accelerometer_data_rate`` and ``gyro_data_rate``"""
+
+
+Mode.add_values(
+    (
+        ("SHUTDOWN", 0, "Shutdown", None),
+        ("NORMAL", 1, "Normal", None),
+        (
+            "LOW_POWER",
+            2,
+            "Low Power",
+            None,
+        ),  # Low power is from 2-6 so checks against this should  be 'mode >=LIS331_MODE_LOW_POWER'
+    )
+)
 
 
 class LIS331:
@@ -176,19 +153,125 @@ class LIS331:
     """
 
     _chip_id = ROUnaryStruct(_LIS331_REG_WHOAMI, "<B")
+    _mode_and_odr = RWBits(5, _LIS331_REG_CTRL1, 3)
     CHIP_ID = None
 
     def __init__(self, i2c_bus, address=_LIS331_DEFAULT_ADDRESS):
         self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
-        # if self.CHIP_ID is None:
-        #     raise AttributeError("LSM6DS Parent Class cannot be directly instantiated")
         if self._chip_id != _LIS331_CHIP_ID:
             raise RuntimeError(
                 "Failed to find %s - check your wiring!" % self.__class__.__name__
             )
-        print("past chip ID check")
+
+        self.data_rate = Rate.RATE_1000_HZ  # pylint: disable=no-member
+        self._cached_accel_range = LIS331HHRange.RANGE_6G  # pylint: disable=no-member
+
+    @property
+    def data_rate(self):
+        """Select the rate at which the accelerometer takes measurements. Must be a `Rate`"""
+        return 1
+
+    @data_rate.setter
+    def data_rate(self, value):
+        self._mode_and_odr = value
+
+    def _mode(self, data_rate):
+        # pylint: disable=no-member
+        pm_value = (data_rate & 0x1C) >> 2
+        if pm_value >= Mode.LOW_POWER:
+            return Mode.LOW_POWER
+
+        return pm_value
+
+    _raw_acceleration = ROByteArray(6, (_LIS331_REG_OUT_X_L | 0x80), "<hhh")
 
     @property
     def acceleration(self):
         """The x, y, z acceleration values returned in a 3-tuple and are in m / s ^ 2."""
-        return (0, 0, 9.8)
+
+        raw_acceleration_bytes = self._raw_acceleration
+
+        return (
+            self._scale_acceleration(raw_acceleration_bytes[0]),
+            self._scale_acceleration(raw_acceleration_bytes[1]),
+            self._scale_acceleration(raw_acceleration_bytes[2]),
+        )
+
+    def _scale_acceleration(self, value):
+        # The measurements are 12 bits left justified to preserve the sign
+        right_justified = value >> 4
+        return right_justified * LIS331HHRange.lsb[self._cached_accel_range]
+
+        # Adafruit_BusIO_Register _ctrl1 = Adafruit_BusIO_Register(
+        #     i2c_dev, spi_dev, ADDRBIT8_HIGH_TOREAD, LIS331_REG_CTRL1, 1);
+        # _ctrl1.write(0x07); // enable all axes, normal mode
+
+        # setDataRate(LIS331_DATARATE_1000_HZ);
+        # setRange(H3LIS331_RANGE_400_G);
+
+        # void Adafruit_LIS331::setDataRate(lis331_data_rate_t data_rate) {
+        # int8_t dr_value = 0;
+        # int8_t pm_value = 0;
+
+        # lis331_mode_t new_mode = getMode(data_rate);
+        # Adafruit_BusIO_Register _ctrl1 = Adafruit_BusIO_Register(
+        #     i2c_dev, spi_dev, ADDRBIT8_HIGH_TOREAD, LIS331_REG_CTRL1, 1);
+        # Adafruit_BusIO_RegisterBits pm_bits =
+        #     Adafruit_BusIO_RegisterBits(&_ctrl1, 3, 5);
+
+        # switch (new_mode) {
+        # case LIS331_MODE_SHUTDOWN:
+        #     break;
+
+        # case LIS331_MODE_LOW_POWER: // ODR bits are in CTRL1[7:5] (PM)
+        #     pm_value = ((data_rate & 0x1C)) >> 2;
+        #     break;
+
+        # case LIS331_MODE_NORMAL: // ODR bits are in CTRL1[4:3] (DR)
+        #     pm_value = ((data_rate & 0x1C)) >> 2;
+        #     dr_value = (data_rate & 0x7);
+
+        #     // only Normal mode uses DR to set ODR, so we can set it here
+        #     Adafruit_BusIO_RegisterBits dr_bits =
+        #         Adafruit_BusIO_RegisterBits(&_ctrl1, 2, 3);
+        #     dr_bits.write(dr_value);
+        #     break;
+        # }
+
+        # pm_bits.write(pm_value);
+        # }
+
+        # /*!
+        # *   @brief  Gets the data rate for the LIS331 (affects power consumption)
+        # *   @return Returns Data Rate value
+        # */
+        # lis331_data_rate_t Adafruit_LIS331::getDataRate(void) {
+        # Adafruit_BusIO_Register _ctrl1 = Adafruit_BusIO_Register(
+        #     i2c_dev, spi_dev, ADDRBIT8_HIGH_TOREAD, LIS331_REG_CTRL1, 1);
+        # Adafruit_BusIO_RegisterBits pm_dr_bits =
+        #     Adafruit_BusIO_RegisterBits(&_ctrl1, 5, 3);
+        # return (lis331_data_rate_t)pm_dr_bits.read();
+        # }
+
+        # /**
+        # * @brief  Return the current power mode from the current data rate
+        # * @return lis331_mode_t The currently set power mode
+        # */
+        # lis331_mode_t Adafruit_LIS331::getMode(void) {
+        # lis331_data_rate_t current_rate = getDataRate();
+        # return getMode(current_rate);
+        # }
+
+        # /**
+        # * @brief Return the current power mode from a given data rate value
+        # *
+        # * @param data_rate The `lis331_data_rate_t` to return the `lis331_mode_t` for
+        # * @return lis331_mode_t
+        # */
+        # lis331_mode_t Adafruit_LIS331::getMode(lis331_data_rate_t data_rate) {
+        # uint8_t pm_value = (data_rate & 0x1C) >> 2;
+        # if (pm_value >= LIS331_MODE_LOW_POWER) {
+        #     return LIS331_MODE_LOW_POWER;
+        # }
+        # return (lis331_mode_t)pm_value;
+        # }
--- examples/lis331_simpletest.py
+++ examples/lis331_simpletest.py
@@ -2,10 +2,12 @@ import time
 import board
 import busio
 import adafruit_lis331
+from adafruit_debug_i2c import DebugI2C
 
 i2c = busio.I2C(board.SCL, board.SDA)
+i2c = DebugI2C(i2c)
 lis = adafruit_lis331.LIS331(i2c)
 
-for i in range(5):
+for i in range(2):
     print("Acceleration : X: %.2f, Y:%.2f, Z:%.2f ms^2" % lis.acceleration)
-    time.sleep(0.3)
+    time.sleep(0.1)
